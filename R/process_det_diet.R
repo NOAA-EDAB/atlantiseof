53c3d298a717cafc7bac15f797e71f625ce2e3cd
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2025-09-24 15:11:40 -0400

    fix chmod and document

--- R/process_det_diet.R
+++ R/process_det_diet.R
@@ -43,7 +43,7 @@ process_det_diet <- function(atl.dir, detDietfile, outputname, cloud =F) {
     
     system(cmd)
     
-    system(paste0('sudo chomd 775 ',atl.dir, outputname))
+    system(paste0('sudo chmod 775 ',atl.dir, outputname))
   }else{
     system2('gzip',args = c('-k',paste0(atl.dir,detDietfile)))
     #then remove zeros and save ans another zip
